9b8015f80c8eb9650a2fbc5f4a9e2e0588b6d770
Letitia Ho <letitiayhho@gmail.com>
2021-08-30 13:01:18 -0500

    Clean up test script

--- test/test.m
+++ test/test.m
@@ -1,25 +1,19 @@
 %% Set up
 cd('~/src/speeded_target_identification/')
 addpath('functions')
-addpath('stim/test_words')
 PsychJavaTrouble(1);
 
 % Add constants
 FS = 44100;
 SUBJ_NUM = 0;
-BLOCK = 0;
-ptb = init_psychtoolbox(FS);
+BLOCK = 'test';
 TRAINING = true;
 
-%% Test present_stimulus.m once
-% stim = get_filepaths('stim/test_words');
-% fixation(ptb);
-% present_stimulus(stim{1}, BLOCK, ptb); % trigger sent here
-% response = ask_for_response(ptb);
-% fprintf(response)
+% Init psychtoolbox
+ptb = init_psychtoolbox(FS);
 
 %% Test present_stimulus.m multiple times
-[fullpath, word] = get_filepaths('stim/test_words');
+[fullpath, word] = get_filepaths(fullfile('stim', BLOCK));
 fixation(ptb); % shows fixation cross to start trial
 for s = 1:length(fullpath)
     [stim_start, stim_end, pressed, rt, resp] = present_stimulus(fullpath{s}, BLOCK, ptb); % trigger sent here

c90f8812f708b47d573f28c07ca4837b823cd93f
Letitia Ho <letitiayhho@gmail.com>
2021-08-30 11:03:05 -0500

    Directly write word instead of passing in filepath

--- functions/write_output.m
+++ functions/write_output.m
@@ -1,13 +1,11 @@
-function write_output(subject, block, trial, stim, stim_start, stim_end, pressed, rt, resp, correct)
+function write_output(subject, block, trial, word, stim_start, stim_end, pressed, rt, resp, correct)
 
     % get output filename for this subject and black
     fpath = ['output/subj' num2str(subject) 'block' num2str(block) '.csv'];
 
-    stim = split(stim, '/');
-    word = char(stim(end));
     row = {num2str(subject), num2str(block), num2str(trial), word,...
         num2str(stim_start), num2str(stim_end), num2str(pressed),...
-        num2str(rt), num2str(resp), num1str(correct)};
+        num2str(rt), num2str(resp), num2str(correct)};
     row = strjoin(row, ',');
     row = ['\n' row];
     

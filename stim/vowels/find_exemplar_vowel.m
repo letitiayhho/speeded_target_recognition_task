4a76c964d1e5315986a0c906d86e3db4a81046bd
Letitia Ho <letitiayhho@gmail.com>
2021-09-01 11:42:30 -0500

    Rank prototypicality of utterances for one subject

--- stim/vowels/find_exemplar_vowel.m
+++ stim/vowels/find_exemplar_vowel.m
@@ -11,20 +11,32 @@ vowels = unique(vowel);
 clear raw vowel f1 f2
 
 % Identify the mean f1 and f2 for each vowel, the prototype
-prototype = grpstats(df, 'vowel');
-% mean_f1 = zeros(length(vowels), 1);
-% mean_f2 = zeros(length(vowels), 1);
-% for i = 1:length(vowels)
-%    vowel = vowels(i);
-%    vowel_f1 = df.f1(strcmp(df.vowel, vowel),:);
-%    vowel_f2 = df.f2(strcmp(df.vowel, vowel),:);
-%     mean_f1(i) = mean(vowel_f1);
-%     mean_f2(i) = mean(vowel_f2);
-% end
-% mean_formants = table(vowels, mean_f1, mean_f2);
+prototypes = grpstats(df, 'vowel');
 
 % Find distance between each utterance and the prototype
+df.d = zeros(size(df, 1), 1);
+for i = 1:size(df, 1)
+    % get the points
+    utterance = df(i, :);
+    prototype = prototypes(strcmp(prototypes.vowel, utterance.vowel),:);
 
+    % compute distance
+    x = [utterance.f1, utterance.f2];
+    y = [prototype.mean_f1, prototype.mean_f2];
+    df.d(i) = sum((x-y).^2).^0.5;
+end
 
 % Rank the utterances by distance
+rank = [];
+for i = 1:size(vowels, 1)
+    distances = df.d(strcmp(df.vowel, vowels(i)),:);
+    rank = [rank; tiedrank(distances)];
+end
+df.ranks = rank;
 
+% Construct filename
+order = repmat([""; num2str((1:9)')], 7, 1);
+df.filename = strcat(char(df.vowel), order, ".wav");
+
+% Save
+writetable(df, 'S_20/prototypicality_rankings.txt')

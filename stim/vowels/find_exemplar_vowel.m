2632a4191a9cc5e8b5ebb2976324ce407ea92ccc
Letitia Ho <letitiayhho@gmail.com>
2021-09-01 12:24:20 -0500

    Wrap in a function

--- stim/vowels/find_exemplar_vowel.m
+++ stim/vowels/find_exemplar_vowel.m
@@ -1,42 +1,45 @@
-% Load data
-cd ~/src/speeded_target_identification/stim/vowels/
-raw = tdfread('S_20/subj20-1.txt');
+function find_exemplar_vowel(subject_number)
+    % Load data
+    cd ~/src/speeded_target_identification/stim/vowels/
+    subject_dir = "S_" + subject_number;
+    raw = tdfread(fullfile(subject_dir, 'subj' + subject_number + '-1.txt'));
 
-% Create a data frame with f1 and f2
-vowel = cellstr(raw.Type);
-f1 = raw.f1;
-f2 = raw.f2;
-df = table(vowel, f1, f2);
-vowels = unique(vowel);
-clear raw vowel f1 f2
+    % Create a data frame with f1 and f2
+    vowel = cellstr(raw.Type);
+    f1 = raw.f1;
+    f2 = raw.f2;
+    df = table(vowel, f1, f2);
+    vowels = unique(vowel);
+    clear raw vowel f1 f2
 
-% Identify the mean f1 and f2 for each vowel, the prototype
-prototypes = grpstats(df, 'vowel');
+    % Identify the mean f1 and f2 for each vowel, the prototype
+    prototypes = grpstats(df, 'vowel');
 
-% Find distance between each utterance and the prototype
-df.d = zeros(size(df, 1), 1);
-for i = 1:size(df, 1)
-    % get the points
-    utterance = df(i, :);
-    prototype = prototypes(strcmp(prototypes.vowel, utterance.vowel),:);
+    % Find distance between each utterance and the prototype
+    df.d = zeros(size(df, 1), 1);
+    for i = 1:size(df, 1)
+        % get the points
+        utterance = df(i, :);
+        prototype = prototypes(strcmp(prototypes.vowel, utterance.vowel),:);
 
-    % compute distance
-    x = [utterance.f1, utterance.f2];
-    y = [prototype.mean_f1, prototype.mean_f2];
-    df.d(i) = sum((x-y).^2).^0.5;
+        % compute distance
+        x = [utterance.f1, utterance.f2];
+        y = [prototype.mean_f1, prototype.mean_f2];
+        df.d(i) = sum((x-y).^2).^0.5;
+    end
+
+    % Rank the utterances by distance
+    rank = [];
+    for i = 1:size(vowels, 1)
+        distances = df.d(strcmp(df.vowel, vowels(i)),:);
+        rank = [rank; tiedrank(distances)];
+    end
+    df.rank = rank;
+
+    % Construct filename
+    order = repmat([""; num2str((1:9)')], 7, 1);
+    df.filename = strcat(char(df.vowel), order, ".wav");
+
+    % Save
+    writetable(df, fullfile(subject_dir, 'prototypicality_rankings.txt'))
 end
-
-% Rank the utterances by distance
-rank = [];
-for i = 1:size(vowels, 1)
-    distances = df.d(strcmp(df.vowel, vowels(i)),:);
-    rank = [rank; tiedrank(distances)];
-end
-df.ranks = rank;
-
-% Construct filename
-order = repmat([""; num2str((1:9)')], 7, 1);
-df.filename = strcat(char(df.vowel), order, ".wav");
-
-% Save
-writetable(df, 'S_20/prototypicality_rankings.txt')

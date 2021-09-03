b319fa0844793a79cefd08aa9e13dbc79d9bc701
Letitia Ho <letitiayhho@gmail.com>
2021-09-03 18:28:20 -0500

    Normalize rms to 1

---
+++ create-stim/normalize_stim_rms.m
@@ -0,0 +1,18 @@
+% NORMALIZE BEFORE ZERO PADDING
+
+dirs = dir('subj*/exemplars/*.wav');
+path = {dirs(:).folder};
+filename = {dirs(:).name};
+
+% loop over each stim
+for i = 1:length(path)
+    stim = fullfile(char(path(i)), char(filename(i)));
+    fprintf(1, [stim, '\n'])
+
+    % zero pad
+    [y, Fs] = audioread(stim);
+    y_new = y*1/(rms(y));
+    
+    % write
+    audiowrite(stim, y_new, Fs)
+end

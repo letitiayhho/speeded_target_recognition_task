205a9014a0cbd8be87f10ca4d0e3873814a96714
Letitia Ho <letitiayhho@gmail.com>
2021-09-02 22:54:43 -0500

    Wrap all scripts into functions

--- stim/get_full_stim_path.m
+++ stim/get_full_stim_path.m
@@ -1,13 +1,13 @@
-n_talkers = 2;
+function get_full_stim_path(n_talkers)
+    % Get talker order
+    talker_order = get_talker_order(n_talkers);
 
-% Get talker order
-talker_order = get_talker_order(n_talkers)
+    % Get vowel order
+    vowel_order = get_vowel_order();
 
-% Get vowel order
-vowel_order = get_vowel_order()
+    % Get full path
+    full_stim_paths = fullfile(talker_order, vowel_order);
 
-% Get full path
-full_stim_paths = fullfile(talker_order, vowel_order);
-
-% Save to .txt file
-writematrix(full_stim_paths, 'stim_paths.txt')
+    % Save to .txt file
+    writematrix(full_stim_paths, 'stim_paths.txt')
+end
--- stim/get_talker_order.m
+++ stim/get_talker_order.m
@@ -1,11 +1,10 @@
-% Number of talkers to select
-n_talkers = 2;
+function [talker_order] = get_talker_order(n_talkers)
+    % Possible talkers
+    talkers = "talker" + [1:12];
 
-% Possible talkers
-talkers = "talker" + [1:12];
+    % Select the talkers
+    talkers = talkers(randi(length(talkers), 1, n_talkers));
 
-% Select the talkers
-talkers = talkers(randi(length(talkers), 1, n_talkers));
-
-% Sample from the talkers 16 times
-talker_order = datasample(talkers, 16)
+    % Sample from the talkers 16 times
+    talker_order = datasample(talkers, 16);
+end

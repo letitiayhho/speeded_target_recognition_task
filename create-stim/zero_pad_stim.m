ce10a17f7fce0059fa464b38a03b9c8f8aff30f9
Letitia Ho <letitiayhho@gmail.com>
2021-09-03 17:42:26 -0500

    Successfully execute over all stim

--- create-stim/zero_pad_stim.m
+++ create-stim/zero_pad_stim.m
@@ -1,12 +1,17 @@
-function zero_pad_stim()
-    % stim directory
-    
-    % loop over each stim
-    
+dirs = dir('subj*/exemplars/*.wav');
+path = {dirs(:).folder};
+filename = {dirs(:).name};
+
+% loop over each stim
+for i = 1:length(path)
+    stim = fullfile(char(path(i)), char(filename(i)));
+    fprintf(1, [stim, '\n'])
+
     % zero pad
-    [aud, Fs] = audioread(stim);
-    wavdata = zeros(length(aud)+Fs*4,1);
-    
+    [y, Fs] = audioread(stim);
+    y_new = zeros(Fs/2,1);
+    y_new(1:length(y)) = y;
+
     % write
-    audiowrite(filename,y,Fs)
+    audiowrite(stim, y_new, Fs)
 end

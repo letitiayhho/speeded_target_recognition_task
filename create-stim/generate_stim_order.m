1b42833cd3935bc0420fe40de46e10570f19d711
Letitia Ho <letitiayhho@gmail.com>
2021-09-02 21:45:05 -0500

    Change folder to create-stim

---
+++ create-stim/generate_stim_order.m
@@ -0,0 +1,15 @@
+% Generate stim order
+possible_distractors = ["AE", "AH", "EE", "EH", "IH", "OO", "UH"];
+possible_targets = ["IH", "OO", "UH"];
+
+% Randomly select target
+target = possible_targets(randi(length(possible_targets), 1));
+
+% Remove target from list of distractors
+possible_distractors(find(strcmp(possible_distractors, target))) = [];
+
+% Pick 16 distractors
+distractors = possible_distractors(randi(length(possible_distractors), 1, 16));
+
+% Replace 4 random distractors with the target
+distractors(datasample([1:16], 4)) = target

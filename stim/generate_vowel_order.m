4a841f324d68f1d4dbda53756b7fbf9e1eebfa66
Letitia Ho <letitiayhho@gmail.com>
2021-09-02 22:35:16 -0500

    Generate full file name, move script

--- stim/generate_vowel_order.m
+++ stim/generate_vowel_order.m
@@ -6,10 +6,13 @@ possible_targets = ["IH", "OO", "UH"];
 target = possible_targets(randi(length(possible_targets), 1));
 
 % Remove target from list of distractors
-possible_distractors(find(strcmp(possible_distractors, target))) = [];
+possible_distractors(strcmp(possible_distractors, target)) = [];
 
 % Pick 16 distractors
 distractors = possible_distractors(randi(length(possible_distractors), 1, 16));
 
 % Replace 4 random distractors with the target
-distractors(datasample([1:16], 4)) = target
+distractors(datasample([1:16], 4)) = target;
+
+% Generate filenames
+stim = distractors + ".wav";

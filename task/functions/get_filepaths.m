660f0fdd7d45fc2a79b8293156ef7564c2377362
Letitia Ho <letitiayhho@gmail.com>
2021-09-05 09:55:55 -0500

    No need to return vowel identity

--- task/functions/get_filepaths.m
+++ task/functions/get_filepaths.m
@@ -1,4 +1,3 @@
-function [fullpath, vowel] = get_filepaths(stim_order)
+function [fullpath] = get_filepaths(stim_order)
     fullpath = fullfile('stim', stim_order.talker, stim_order.vowel + ".wav");
-    vowel = stim_order.vowel;
 end

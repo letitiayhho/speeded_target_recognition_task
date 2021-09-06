9def674fec90b1e37c40c8f535c44c9e1121a086
Letitia Ho <letitiayhho@gmail.com>
2021-09-06 16:57:12 -0500

    Create script that only plays one vowel over and over

---
+++ task/test/test_rtbox.m
@@ -0,0 +1,20 @@
+%% Set up
+cd('~/src/speeded_vowel_identification/')
+addpath('task/functions')
+PsychJavaTrouble(1);
+
+% Add constants
+FS = 44100;
+SUBJ_NUM = 0;
+BLOCK = 'test';
+TRAINING = true; % change training depending on block number
+
+%% Init psychtoolbox
+ptb = init_psychtoolbox(FS);
+
+%% Test with same audio each time
+stim = 'f1/AH/wav';
+fixation(ptb); % show fixation cross to start trial
+for s = 1:20
+    [rt, resp] = present_stimulus(stim, ptb); % trigger sent here
+end

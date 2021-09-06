71696b5dba0181ce6dea21117770a6e3cefd7206
Letitia Ho <letitiayhho@gmail.com>
2021-09-06 11:49:08 -0500

    Rename 'word' var to 'vowel', update filepath

--- task/functions/write_output.m
+++ task/functions/write_output.m
@@ -1,16 +1,16 @@
-function write_output(subject, block, trial, word, stim_start, stim_end, pressed, rt, resp, correct)
+function write_output(subject, block, trial, vowel, stim_start, stim_end, pressed, rt, resp, correct)
 
     % get output filename for this subject and black
-    fpath = ['output/subj' num2str(subject) 'block' num2str(block) '.csv'];
+    fpath = ['task/output/subj' num2str(subject) 'block' num2str(block) '.csv'];
 
-    row = {num2str(subject), num2str(block), num2str(trial), word,...
+    row = {num2str(subject), num2str(block), num2str(trial), char(vowel),...
         num2str(stim_start), num2str(stim_end), num2str(pressed),...
         num2str(rt), num2str(resp), num2str(correct)};
     row = strjoin(row, ',');
     row = ['\n' row];
     
     if ~(exist(fpath, 'file') == 2) % ~isfile(fpath) in later versions
-        cols = 'subject,block,trial,word,stim_start,stim_end,pressed,rt,resp,correct';
+        cols = 'subject,block,trial,vowel,stim_start,stim_end,pressed,rt,resp,correct';
         row = [cols row];
         f = fopen(fpath, 'wt'); 
         fprintf(f, row);

4e9867fc11026352e6744d0dcc3d728403f3e05a
Letitia Ho <letitiayhho@gmail.com>
2021-09-06 11:47:32 -0500

    Catch empty answers

--- task/functions/check_answer.m
+++ task/functions/check_answer.m
@@ -1,9 +1,12 @@
-function [correct] = check_answer(word, resp)
-    first_char = word(1);
-    
-    if first_char == resp
+function [correct] = check_answer(istarget, resp)
+    istarget = logical(istarget);
+    if resp == "" % catch empty answers
+        correct = 0;
+    elseif istarget && resp == 't' % correctly identifies target
         correct = 1;
-    else
+    elseif not(istarget) && resp == 'n' % correctly identifies non-targets
+        correct = 1;
+    else % all other cases
         correct = 0;
     end
 end

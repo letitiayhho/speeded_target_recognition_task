e2e15ad18cc2781127af42f4a39882b61c881d8f
Letitia Ho <letitiayhho@gmail.com>
2021-09-06 10:37:52 -0500

    present_stimulus works now, remember to CLEAR VARS before each run

--- task/test/test_present_stimulus.m
+++ task/test/test_present_stimulus.m
@@ -1,3 +1,8 @@
+sca;
+close all;
+clearvars;
+PsychPortAudio('Close'); % clear audio handles
+
 %% Set up
 cd('~/src/speeded_vowel_identification/')
 addpath('task/functions')
@@ -6,12 +11,52 @@ addpath('task/functions')
 Fs = 44100;
 BLOCK = 0;
 stim = 'stim/f1/OO.wav';
-[aud, ~] = audioread(stim);
+ptb = init_psychtoolbox(Fs);
+
+[stim_start, stim_end, pressed, rt, resp] = present_stimulus(stim, ptb)
+% 
+% %% Test current script
+% [aud, ~] = audioread(stim);
+% PsychPortAudio('FillBuffer', ptb.pahandle, [aud'; aud']);
+% 
+% % inter-trial interval
+% jitter = rand/5 - .1; % -100 to +100 ms uniform jitter
+% WaitSecs(.5 + jitter);
+% 
+% % start collecting response
+% KbQueueCreate(ptb.keyboard);
+% ListenChar(2);
+% KbQueueStart;
+% 
+% % play audio
+% t0 = GetSecs + .001;
+% PsychPortAudio('Start', ptb.pahandle, 1, t0, 1);
+% 
+% % stop audio
+% [stim_start, ~, ~, stim_end] = PsychPortAudio('Stop', ptb.pahandle, 1, 1);
+% 
+% % Collect response
+% [pressed, rt] = KbQueueCheck;
+% resp = KbName(rt);
+% [rt, I] = min(rt(rt > 0)); % keep only first response
+% resp = char(resp(I));
+% 
+% % Wait
+% WaitSecs(.2 + rand()*.2);
+% KbQueueStop;
+% KbQueueRelease;
+% ListenChar(0); % renables matlab command window
+
+%% Test final version
+% [aud, ~] = audioread(stim);
+% ptb = init_psychtoolbox(Fs);
+% [stim_start, stim_end, pressed, rt, resp] = present_stimulus(stim, BLOCK, ptb);
+% fprintf(1, resp)
 
 %% Test init_psychtoolbox()
 
 % Initialize sound with init_psychtoolbox()
-ptb = init_psychtoolbox(Fs);
+% ptb = init_psychtoolbox(Fs);
 
 % Innards of init_psychtoolbox
 % InitializePsychSound(1);
@@ -25,9 +70,10 @@ ptb = init_psychtoolbox(Fs);
 % % [actualStartTime, ~, ~, estStopTime] = PsychPortAudio('Stop', ptb.pahandle, 1, 1);
 
 % Johns version
-[stim_start, stim_end, pressed, rt, resp] = present_stimulus(stim, BLOCK, ptb);
+% [stim_start, stim_end, pressed, rt, resp] = present_stimulus(stim, BLOCK, ptb);
+% fprintf(1, resp)
 
-% Innards of Johns version
+% Innards of Johns versionttttt
 % PsychPortAudio('FillBuffer', ptb.pahandle, [aud'; aud']);
 % t0 = GetSecs + .001;
 % starttime = PsychPortAudio('Start', ptb.pahandle, 1, t0, 1);

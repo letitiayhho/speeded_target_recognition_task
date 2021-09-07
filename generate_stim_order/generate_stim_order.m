4727e67bb5a700420488dd4246e88238985b3cae
Letitia Ho <letitiayhho@gmail.com>
2021-09-07 14:59:44 -0500

    Wrap with function that takes 'subject_number' as an input

--- generate_stim_order/generate_stim_order.m
+++ generate_stim_order/generate_stim_order.m
@@ -1,34 +1,36 @@
-cd '/Users/letitiaho/src/speeded_vowel_identification/generate_stim_order/'
-addpath('functions')
-subject_number = 0;
+function generate_stim_order(subject_number)
+    cd '/Users/letitiaho/src/speeded_vowel_identification/generate_stim_order/'
+    addpath('functions')
+    subject_number = str2num(subject_number);
 
-% SET SEED
-rng(subject_number)
+    % SET SEED
+    rng(subject_number)
 
-% Get talker order
-[type, talker] = get_talker_order(subject_number);
+    % Get talker order
+    [type, talker] = get_talker_order();
 
-% Get vowel order
-[vowel, istarget] = get_vowel_order();
+    % Get vowel order
+    [vowel, istarget] = get_vowel_order();
 
-% Block 
-block_length = [1, 5, 16, 16, 16, 16];
-block = [];
-rep = [];
-utterance = [];
-for i = 1:length(block_length)
-    block = [block; repmat(i, block_length(i)*16, 1)];
-    for j = 1:block_length(i)
-        rep = [rep; repmat(j, 16, 1)];
-        utterance = [utterance; (1:16)'];
+    % Block 
+    block_length = [1, 5, 16, 16, 16, 16];
+    block = [];
+    rep = [];
+    utterance = [];
+    for i = 1:length(block_length)
+        block = [block; repmat(i, block_length(i)*16, 1)];
+        for j = 1:block_length(i)
+            rep = [rep; repmat(j, 16, 1)];
+            utterance = [utterance; (1:16)'];
+        end
     end
+
+    % Subject
+    subject = repmat(subject_number, length(type), 1);
+
+    % COLUMNS: subject, block, type, rep, utterance, vowel, istarget, talker
+    stim_order = table(subject, block, type, rep, utterance, vowel, istarget, talker);
+
+    % WRITE
+    writetable(stim_order, ['output/', num2str(subject_number), '_stim_order.txt'])
 end
-
-% Subject
-subject = repmat(subject_number, length(type), 1);
-
-% COLUMNS: subject, block, type, rep, utterance, vowel, istarget, talker
-stim_order = table(subject, block, type, rep, utterance, vowel, istarget, talker);
-
-% WRITE
-writetable(stim_order, ['output/', num2str(subject_number), '_stim_order.txt'])

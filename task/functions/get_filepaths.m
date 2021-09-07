e3754c4b13971936f45de081f24a60841d52ddbe
Letitia Ho <letitiayhho@gmail.com>
2021-09-07 16:17:23 -0500

    Pass in block and rep number to get stim paths and target for that rep

--- task/functions/get_filepaths.m
+++ task/functions/get_filepaths.m
@@ -1,3 +1,5 @@
-function [fullpath] = get_filepaths(stim_order)
+function [fullpath] = get_filepaths(stim_order, block, rep)
+    stim_order = stim_order(stim_order.block == block, :);
+    stim_order = stim_order(stim_order.rep == rep, :);
     fullpath = fullfile('stim', stim_order.talker, stim_order.vowel + ".wav");
 end
--- task/functions/get_target.m
+++ task/functions/get_target.m
@@ -1,4 +1,6 @@
-function [target] = get_target(stim_order)
+function [target] = get_target(stim_order, block, rep)
+    stim_order = stim_order(stim_order.block == block, :);
+    stim_order = stim_order(stim_order.rep == rep, :);
     target = unique(stim_order.vowel(logical(stim_order.istarget)));
     target = char(target);
 end

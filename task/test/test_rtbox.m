f73231c728acc789ac418fffb116175a21014145
Letitia Ho <letitiayhho@gmail.com>
2021-09-09 13:53:15 -0500

    Init RTBox

--- task/test/test_rtbox.m
+++ task/test/test_rtbox.m
@@ -1,21 +1,26 @@
+PsychDebugWindowConfiguration
+
 %% Set up
 cd('~/src/speeded_vowel_identification/')
 addpath('task/functions')
+addpath('task/USTCRTBox_003')      
 PsychJavaTrouble(1);
 
 % Add constants
 FS = 44100;
 SUBJ_NUM = 0;
 BLOCK = 'test';
+RTBOX = true;
 TRAINING = true; % change training depending on block number
 
-%% Init psychtoolbox
+% set up psychtoolbox and RTBox
+init_RTBox(RTBOX);
 ptb = init_psychtoolbox(FS);
 
 %% Test with same audio each time
 stim = 'stim/f1/AH.wav';
 fixation(ptb); % show fixation cross to start trial
-for s = 1:20
+for s = 1:100
     [rt, resp] = present_stimulus(stim, ptb); % trigger sent here
 end
 sca

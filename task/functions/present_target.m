f97e4f0c0fd55b8181dacde1bb3405f4fb7d0dd4
Letitia Ho <letitiayhho@gmail.com>
2021-09-13 12:07:05 -0500

    Present target for 1 sec instead of 750 ms

--- task/functions/present_target.m
+++ task/functions/present_target.m
@@ -1,9 +1,9 @@
 function present_target(ptb, target)
 
-    % show fixation cross for 750 ms and then clear
+    % show fixation cross for 1 sec and then clear
     DrawFormattedText(ptb.window, target, 'center', 'center', 1);
     Screen('Flip', ptb.window);
-    WaitSecs(.75);
+    WaitSecs(1);
     Screen('Flip', ptb.window);
     
 end

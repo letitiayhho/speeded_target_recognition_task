93cb50097cb1ab873c439bf9b0abc34bbf548597
Letitia Ho <letitiayhho@gmail.com>
2021-09-13 12:31:28 -0500

    Simplify how details of the stim of each rep are retrieved

---
+++ task/functions/get_rep_stim.m
@@ -0,0 +1,10 @@
+function [vowels, paths, target] = get_filepaths(stim_order, block, rep)
+    % subset by block and rep
+    stim_order = stim_order(stim_order.block == block, :);
+    stim_order = stim_order(stim_order.rep == rep, :);
+    
+    % get details
+    vowels = char(stim_order.vowel);
+    paths = fullfile('stim', stim_order.talker, stim_order.vowel + ".wav");
+    target = char(unique(stim_order.vowel(logical(stim_order.istarget))));
+end
--- task/run.m
+++ task/run.m
@@ -20,42 +20,42 @@ IS_TRAINING = BLOCK == 1 || BLOCK == 2; % change training depending on block num
 
 % set up psychtoolbox and RTBox
 init_RTBox(RTBOX);
-PTB = init_psychtoolbox(FS);
+% PTB = init_psychtoolbox(FS);
 
 % Load stim order
 stim_file = ['generate_stim_order/output/', num2str(SUBJ_NUM), '_stim_order.txt'];
 STIM = readtable(stim_file);
 
 %% Display instructions
-instructions(PTB, BLOCK);
+% instructions(PTB, BLOCK);
 
 %% Task
 % loop through all reps in block
-STIM = STIM(STIM.block == BLOCK, :);
-n_reps = max(STIM.rep);
+n_reps = get_n_reps(STIM, BLOCK);
+
 for rep = 1:n_reps
-    stim_path = get_filepaths(STIM, BLOCK, rep);
-    target = get_target(STIM, BLOCK, rep);
-    fprintf(1, target)
+    [vowels, paths, target] = get_rep_stim(STIM, BLOCK, rep);
 
     % loop through all stim in rep
-    fixation(PTB); % show fixation cross to start trial
-    present_target(PTB, target) % show target
-
+%     fixation(PTB); % show fixation cross to start trial
+%     present_target(PTB, target) % show target
+% 
     for v = 1:length(stim_path)
-        [rt, resp] = present_stimulus(stim_path(v), PTB); % trigger sent here
-        correct = check_answer(STIM.istarget(v), resp);
-        write_output(SUBJ_NUM, BLOCK, v, STIM.vowel(v), rt, resp, correct);
-        if IS_TRAINING
-            give_feedback(correct, PTB);
-        end
+        fprintf(1, [paths(v)+ '\n'])
+        fprintf(1, [vowels(v, :), '\n'])
+%         [rt, resp] = present_stimulus(stim_path(v), PTB); % trigger sent here
+%         correct = check_answer(STIM.istarget(v), resp);
+%         write_output(SUBJ_NUM, BLOCK, v, STIM.vowel(v), rt, resp, correct);
+%         if IS_TRAINING
+%             give_feedback(correct, PTB);
+%         end
     end
 end
 
 %% end block
-instructions(PTB, 0)
+% instructions(PTB, 0)
 
 sca; % screen clear all
 close all;
-clearvars;
+% clearvars;
 PsychPortAudio('Close'); % clear audio handles

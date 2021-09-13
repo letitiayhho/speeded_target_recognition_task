d945171e84cf6ef1620993cdb21b00cd36709a4d
Letitia Ho <letitiayhho@gmail.com>
2021-09-13 13:30:43 -0500

    Keep fixation until keypress

--- task/functions/fixation.m
+++ task/functions/fixation.m
@@ -3,8 +3,7 @@ function fixation(ptb)
     % show fixation cross for 1 sec and then clear
     DrawFormattedText(ptb.window, '+', 'center', 'center', 1);
     Screen('Flip', ptb.window);
-    WaitSecs(1);
-    Screen('Flip', ptb.window);
     KbPressWait();
+    Screen('Flip', ptb.window);
     
 end

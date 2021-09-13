5fb12b9010b1fe562cc3b3208f363266563fdd3c
Letitia Ho <letitiayhho@gmail.com>
2021-09-13 13:30:52 -0500

    Present target for longer

--- task/functions/present_target.m
+++ task/functions/present_target.m
@@ -3,7 +3,7 @@ function present_target(ptb, target)
     % show fixation cross for 1 sec and then clear
     DrawFormattedText(ptb.window, target, 'center', 'center', 1);
     Screen('Flip', ptb.window);
-    WaitSecs(1);
+    WaitSecs(2);
     Screen('Flip', ptb.window);
     
 end

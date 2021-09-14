10f3c195783dbdbb16e3e987b6df396a0af7be01
Letitia Ho <letitiayhho@gmail.com>
2021-09-14 10:09:23 -0500

    Make each speaker appear an equal number of times per rep

--- generate_stim_order/functions/get_talker_order.m
+++ generate_stim_order/functions/get_talker_order.m
@@ -6,6 +6,7 @@ function [block_type, talker] = get_talker_order()
     block_type = [];
     for i = 1:length(rep_talkers)
         block_type = [block_type; repmat(rep_talkers(i, 3), 16, 1)];
-        talker = [talker; datasample(rep_talkers(i, 1:2), 16)'];
+        sample = repmat(rep_talkers(i, 1:2), 1, 8);
+        talker = [talker; datasample(sample, 16, 'Replace', false)'];
     end
 end

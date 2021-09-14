944efd0dacdd2bf9d049858cfbcb02f3953eb49a
Letitia Ho <letitiayhho@gmail.com>
2021-09-14 10:07:38 -0500

    Make each target appear an approximately euqal number of times, make repeats of a single target more than twice in a row impossible

--- generate_stim_order/functions/choose_target_for_each_rep.m
+++ generate_stim_order/functions/choose_target_for_each_rep.m
@@ -1,4 +1,8 @@
 function target_order = choose_target_for_each_rep()
     possible_targets = ["IH", "OO", "UH"];
-    target_order = datasample(possible_targets, 70);
+    target_order = [];
+    for i = 1:24
+        target_order = [target_order, datasample(possible_targets, 3, 'Replace', false)];
+    end
+    target_order = target_order(1:70);
 end

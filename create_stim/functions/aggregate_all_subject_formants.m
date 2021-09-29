f53bfe3b05932133b8eafd4dae87abf419825bd0
Letitia Ho <letitiayhho@gmail.com>
2021-09-29 18:21:10 -0500

    Add vowel duration in seconds to all_formants info

--- create_stim/functions/aggregate_all_subject_formants.m
+++ create_stim/functions/aggregate_all_subject_formants.m
@@ -1,31 +1,38 @@
 %% Get f1 and f2 values of all subjects
-cd ~/src/speeded_vowel_identification/create_stim
+cd ~/src/speeded_vowel_identification_task/create_stim
 subjects = dir('subj*');
 all_formants = table();
 
 for i = 1:length(subjects)
     subject_dir = subjects(i).name;
     raw = readtable(fullfile(subject_dir, subject_dir + "-1.txt"), 'ReadVariableNames', true);
+    raw = sortrows(raw, 3);
     
-    % Swamp "subj9" for "subj09"
+    % Get vowel durations
+    [secs, file_order] = compute_vowel_duration(subject_dir);
+    
+    % Swap "subj9" for "subj09"
     if strcmp(subject_dir, "subj9")
         subject_dir = "subj09";
     end
     
+    % Extract relevant fields
     subject = cellstr(repmat(subject_dir, size(raw, 1), 1));
     vowel = cellstr(raw.Type);
     f0 = raw.f0;
     f1 = raw.f1;
     f2 = raw.f2;
     f3 = raw.f3;
-    all_formants = [all_formants; table(subject, vowel, f0, f1, f2, f3)];
     
-    % Remove nans
-    all_formants = all_formants(~any(ismissing(all_formants),2),:);
-    
-    % Sort by subject
-    all_formants = sortrows(all_formants);
+    % Add to table
+    all_formants = [all_formants; table(subject, vowel, f0, f1, f2, f3, secs)];
 end
+
+% Remove nans
+all_formants = all_formants(~any(ismissing(all_formants),2),:);
+    
+% Sort by subject
+all_formants = sortrows(all_formants, 1);
 writetable(all_formants, 'all_formants.txt')
 
 
---
+++ create_stim/functions/compute_vowel_duration.m
@@ -0,0 +1,11 @@
+function [secs, file_order] = compute_vowel_duration(subject_dir)
+    paths = dir(fullfile('.', subject_dir, 'all_vowels/*.wav'));
+    secs = zeros(length(paths), 1);
+    file_order = [];
+    for i = 1:length(paths)
+        filepath = fullfile(paths(i).folder, paths(i).name);
+        info = audioinfo(filepath);
+        secs(i) = info.Duration;
+        file_order = [file_order; cellstr(filepath)];
+    end
+end

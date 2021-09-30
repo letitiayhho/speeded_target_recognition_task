76eca3d3e2176993c90ae13b3ac476b0e82b42c0
Letitia Ho <letitiayhho@gmail.com>
2021-09-30 14:59:41 -0500

    Turn into function

--- create_stim/functions/normalize_stim_rms.m
+++ create_stim/functions/normalize_stim_rms.m
@@ -1,11 +1,8 @@
-% NORMALIZE BEFORE ZERO PADDING
-
 cd('/Users/letitiaho/src/speeded_vowel_identification_task')
 dirs = dir('create_stim/subj*/exemplars/*.wav');
 path = {dirs(:).folder};
 filename = {dirs(:).name};
 
-% RMS = [];
 % loop over each stim
 for i = 1:length(path)
     stim = fullfile(char(path(i)), char(filename(i)));
@@ -13,11 +10,26 @@ for i = 1:length(path)
 
     % change rms to 0.25
     [y, Fs] = audioread(stim);
-%     RMS = [RMS, rms(y)];
     y_new = y*0.25/(rms(y));
     
     % write
     audiowrite(stim, y_new, Fs)
 end
 
-% hist(RMS)
+% function [] = normalize_stim_rms(subject_dir, vowel)
+% arguments
+%     subject_dir char
+%     vowel char
+% end
+% 
+%     stim = fullfile(subject_dir, "exemplars", strcat(vowel, ".wav"));
+% 
+%     % change rms to 0.25
+%     fprintf(1, stim)
+% %     [y, Fs] = audioread(stim);
+% %     RMS = [RMS, rms(y)];
+% %     y_new = y*0.25/(rms(y));
+% %     
+% %     % write
+% %     audiowrite(stim, y_new, Fs)
+% end

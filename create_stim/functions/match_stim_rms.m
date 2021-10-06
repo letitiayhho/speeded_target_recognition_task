de3b58358862a4683fa592347a5d60a5dc53006d
Letitia Ho <letitiayhho@gmail.com>
2021-10-06 15:43:09 -0500

    Match RMS to 0.05 instead of 0.25, rename from "normalize" to "match"

--- create_stim/functions/match_stim_rms.m
+++ create_stim/functions/match_stim_rms.m
@@ -7,9 +7,9 @@ for i = 1:length(path)
     stim = fullfile(char(path(i)), char(filename(i)));
     fprintf(1, [stim, '\n'])
 
-    % change rms to 0.25
+    % change rms to 0.05
     [y, Fs] = audioread(stim);
-    y_new = y*0.25/(rms(y));
+    y_new = y*0.05/(rms(y));
     
     % write
     audiowrite(stim, y_new, Fs)

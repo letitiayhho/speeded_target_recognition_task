c3f86d46306eec3ceedddc2e0e7f13fae57b6140
Letitia Ho <letitiayhho@gmail.com>
2021-10-20 22:10:17 -0500

    Stable SOA of 500 ms regardless of target response

--- task/functions/present_stimulus.m
+++ task/functions/present_stimulus.m
@@ -22,17 +22,16 @@ function [rt, resp] = present_stimulus(stim, ptb)
     % collect response
     pressed = 0;
     times_up = 0;
-    while ~pressed
-        [pressed, rt] = KbQueueCheck(); %check response
+    rt = 0;
+    while ~times_up % ~pressed
+        [pressed, this_rt] = KbQueueCheck(); %check response
         times_up = GetSecs - resp_start > 0.5;
-        if times_up
-            break
+        if pressed
+            rt = this_rt;
         end
     end
     
-%     % ENDS EARLY IF KEY IS PRESSED
-    
-    if pressed
+    if sum(rt) > 0
         keylist = KbName(rt);
         [rt, I] = min(rt(rt > 0)); % keep only first response
         resp = char(keylist(I));
@@ -42,15 +41,8 @@ function [rt, resp] = present_stimulus(stim, ptb)
         rt = [];
     end
 
-    % Wait
-    WaitSecs(.25); % add a jitter using EEG WaitSecs(.2 + rand()*.2)
-    ListenChar(0); % renables matlab command window
-    
-    pressed
-    duration_psychtoolbox = stim_start - GetSecs
-    duration = resp_start - GetSecs
-
     % end of accepting response
+    ListenChar(0); % renables matlab command window
     Screen('Flip', ptb.window);
 
 end

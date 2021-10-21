817b07729f99dedc3a5a5351060c695b25dc3c7d
Letitia Ho <letitiayhho@gmail.com>
2021-10-21 08:54:18 -0500

    Add "please wait" between instructions and start of block

--- task/functions/instructions.m
+++ task/functions/instructions.m
@@ -9,6 +9,7 @@ function instructions(ptb, block)
     end
     
     ListenChar(-1); % disable typing into matlab window
+    
     for i = 1:length(l)
         fpath = ['task/instructions/instructions_' num2str(l(i)) '.txt'];
         txt = load_text_from(fpath);
@@ -17,6 +18,12 @@ function instructions(ptb, block)
         Screen('Flip', ptb.window);
         KbPressWait();
     end
+    
+    fpath = 'task/instructions/wait.txt';
+    txt = load_text_from(fpath);
+    DrawFormattedText(ptb.window, txt, 'center', 'center', 1);
+    Screen('Flip', ptb.window);
+    
     ListenChar(0); % reenable typing into matlab
     
 end

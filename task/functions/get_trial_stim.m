aac77506ed847cc518931dcf77f60b46a5450b19
Letitia Ho <letitiayhho@gmail.com>
2021-10-21 10:00:48 -0500

    Return entire stim_order for that trial

--- task/functions/get_trial_stim.m
+++ task/functions/get_trial_stim.m
@@ -1,11 +1,9 @@
-function [vowel, path, istarget, target, trial_type] = get_trial_stim(stim_order, trial)
+function [stim_order, path, istarget, target] = get_trial_stim(stim_order, trial)
     % subset by block and trial
     stim_order = stim_order(stim_order.trial == trial, :);
     
     % get details
-    vowel = char(stim_order.vowel);
     path = fullfile('stim', stim_order.talker, stim_order.vowel + stim_order.exemplar + ".wav");
     istarget = stim_order.istarget;
     target = char(unique(stim_order.vowel(logical(stim_order.istarget))));
-    trial_type = unique(stim_order.trial_type);
 end

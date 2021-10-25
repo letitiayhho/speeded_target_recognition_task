d5a4954a4217e92f2d687b19704b97ddf1a1fa23
Letitia Ho <letitiayhho@gmail.com>
2021-10-25 16:09:04 -0500

    Revert no response to string nan

--- task/functions/check_answer.m
+++ task/functions/check_answer.m
@@ -1,11 +1,11 @@
 function [correct] = check_answer(istarget, resp)
     istarget = logical(istarget);
 
-    if istarget && ~isnan(resp) % correctly identifies target
+    if istarget && ~strcmp(resp, "nan") % correctly identifies target
         correct = 1;
-    elseif istarget && isnan(resp) % miss a target
+    elseif istarget && strcmp(resp, "nan") % miss a target
         correct = 0;
-    elseif ~istarget && isnan(resp) % correctly skips non-targets
+    elseif ~istarget && strcmp(resp, "nan") % correctly skips non-targets
         correct = 1;
     else % all other cases
         correct = 0;

d6c2d7cc378e99239f11ed18d26920cba4e7e804
Letitia Ho <letitiayhho@uchicago.edu>
2021-10-28 11:35:46 -0500

    Add some files to test RTBox and MATLAB running privileges

---
+++ task/test/isWindowsAdmin.m
@@ -0,0 +1,6 @@
+function out = isWindowsAdmin()
+    %ISWINDOWSADMIN True if this user is in admin role.
+    wi = System.Security.Principal.WindowsIdentity.GetCurrent();
+    wp = System.Security.Principal.WindowsPrincipal(wi);
+    out = wp.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+end

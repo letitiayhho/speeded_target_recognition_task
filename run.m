176e7eaa0420d1e0fe7391dc2e26312fb644afdb
Letitia Ho <letitiayhho@uchicago.edu>
2021-11-08 10:42:09 -0600

    Loop over all blocks

--- run.m
+++ run.m
@@ -1,8 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%% UPDATE THIS SECTION BEFORE EACH SUBJECT/TEST
 
 SUBJ_NUM = 0; % numeric
-BLOCK = 7; % numeric from 1 to 9, 1 is training block
-PILOT = false; % logical
+PILOT = true; % logical
 TEST = false; % logical
 
 %%%%%%%%%%%%%%%%%%%%%%%
@@ -26,40 +25,46 @@ FS = 44100;
 PTB = init_psychtoolbox(FS);
 init_RTBox(RTBOX);
 
-% Load stim order
-[STIM, N_TRIALS] = generate_stim_order(SUBJ_NUM, BLOCK);
+%% Loop over blocks
+BLOCKS = 1:9;
 
-%% Display instructions
-update_instructions(BLOCK)
-instructions(PTB, BLOCK);
+for BLOCK = BLOCKS
+    % Load stim order
+    [STIM, N_TRIALS] = generate_stim_order(SUBJ_NUM, BLOCK);
 
-%% Task
-for trial = 1:N_TRIALS
-    [trial_stim, path, istarget, target] = get_trial_stim(STIM, trial);
+    %% Display instructions
+    update_instructions(BLOCK)
+    instructions(PTB, BLOCK);
 
-    WaitSecs(2);
-    fixation(PTB); % show fixation cross to start trial
-    present_target(PTB, target); % show target
-    tic;
+    %% Task
+    for trial = 1:N_TRIALS
+        [trial_stim, path, istarget, target] = get_trial_stim(STIM, trial);
 
-    % loop through all stim in trial
-    DrawFormattedText(PTB.window, 'x', 'center', 'center', 1);
-    Screen('Flip', PTB.window);
-    for v = 1:length(path) 
-        rt = present_stimulus(path(v), PTB); % trigger sent here
-        write_output(SUBJ_NUM, BLOCK, trial_stim(v,:), rt, PILOT);
-        if BLOCK == 1
-            correct = check_answer(istarget(v), rt);
-            give_feedback(correct, PTB);
+        WaitSecs(2);
+        fixation(PTB); % show fixation cross to start trial
+        present_target(PTB, target); % show target
+        tic;
+
+        % loop through all stim in trial
+        DrawFormattedText(PTB.window, 'x', 'center', 'center', 1);
+        Screen('Flip', PTB.window);
+        for v = 1:length(path) 
+            rt = present_stimulus(path(v), PTB); % trigger sent here
+            write_output(SUBJ_NUM, BLOCK, trial_stim(v,:), rt, PILOT);
+            if BLOCK == 1
+                correct = check_answer(istarget(v), rt);
+                give_feedback(correct, PTB);
+            end
         end
+        Screen('Flip', PTB.window);
     end
-    Screen('Flip', PTB.window);
-end
 
-%% end block
-instructions(PTB, 0) 
+    %% end block
+    instructions(PTB, 0) 
+end
 
 sca; % screen clear all
 close all;
 clearvars;
 PsychPortAudio('Close'); % clear audio handles
+clear all;
